08bbd09ca3ce5eafd4e01c6f108dd3b50f74cc1c
DeepanRaja S <119166130+deepanselvam@users.noreply.github.com>
2023-07-18 11:32:14 +0530

    Update row_sum.py

--- row_sum.py
+++ row_sum.py
@@ -1,16 +1,16 @@
-r = int(input())
-c = int(input())
+a = int(input())
+b = int(input())
 matrix = []
-for i in range(r):
+for i in range(a):
     row = []
-    for j in range(c):
+    for j in range(b):
         element = int(input())
         row.append(element)
     matrix.append(row)
 l = []
-for i in range(r):
+for i in range(a):
     rowsum = 0
-    for j in range(c):
+    for j in range(b):
         rowsum += matrix[i][j]
     l.append(rowsum)
 print(l)
